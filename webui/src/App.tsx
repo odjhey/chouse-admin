4b08ff014d7d34e1c19a99c0d4b5c0cc3636aa79
Odee <odjhey@gmail.com>
2020-07-17 15:24:20 +0800

    feat(webui): add react-admin

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import { Filter, TextInput, List, EmailField, DateField } from "react-admin";
+import { Datagrid, TextField, Pagination } from "react-admin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import buildGraphQLProvider from "ra-data-graphql-simple";
+
+const TaskPagination = (props) => (
+  <Pagination rowsPerPageOptions={[10, 25, 50, 100]} {...props} />
+);
+
+const TaskList = (props) => (
+  <List pagination={<TaskPagination />} filters={<TaskFilter />} {...props}>
+    <Datagrid>
+      <TextField source="description" />
+      <TextField source="story_id" />
+      <DateField source="updated_at" />
+      <TextField source="id" />
+      <TextField source="complete" />
+      <DateField source="created_at" />
+      <DateField source="completed_at" />
+    </Datagrid>
+  </List>
+);
+
+class App extends Component {
+  constructor() {
+    super({});
+    this.state = { dataProvider: null };
+  }
+  componentDidMount() {
+    buildGraphQLProvider({
+      clientOptions: { uri: "http://localhost:4000" },
+    }).then((dataProvider) => this.setState({ dataProvider }));
+  }
+
+  render() {
+    //@ts-ignore
+    const { dataProvider } = this.state;
+
+    if (!dataProvider) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <Admin dataProvider={dataProvider}>
+        <Resource name="Task" list={TaskList} />
+      </Admin>
+    );
+  }
 }
 
 export default App;
+
+const TaskFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+  </Filter>
+);
